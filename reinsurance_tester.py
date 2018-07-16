373b1c58437775457d8b515c77cf36de3407d096
Mark Pinkerton <mark.pinkerton@oasislnf.org>
2018-07-16 16:43:58 +0100

    Fixed typo

--- reinsurance_tester.py
+++ reinsurance_tester.py
@@ -18,7 +18,7 @@ def load_oed_dfs(oed_dir, show_all=False):
     Load OED data files.
     """
 
-    do_reinsuarnce = True
+    do_reinsurance = True
     if oed_dir is not None:
         if not os.path.exists(oed_dir):
             print("Path does not exist: {}".format(oed_dir))
@@ -46,7 +46,7 @@ def load_oed_dfs(oed_dir, show_all=False):
         if not oed_ri_info_file_exists and not oed_ri_scope_file_exists:
             ri_info_df = None
             ri_scope_df = None
-            do_reinsuarnce = False
+            do_reinsurance = False
         elif oed_ri_info_file_exists and oed_ri_scope_file_exists:
             ri_info_df = pd.read_csv(oed_ri_info_file)
             ri_scope_df = pd.read_csv(oed_ri_scope_file)
@@ -56,10 +56,10 @@ def load_oed_dfs(oed_dir, show_all=False):
         if not show_all:
             account_df = account_df[common.OED_ACCOUNT_FIELDS].copy()
             location_df = location_df[common.OED_LOCATION_FIELDS].copy()
-            if do_reinsuarnce:
+            if do_reinsurance:
                 ri_info_df = ri_info_df[common.OED_REINS_INFO_FIELDS].copy()
                 ri_scope_df = ri_scope_df[common.OED_REINS_SCOPE_FIELDS].copy()
-    return (account_df, location_df, ri_info_df, ri_scope_df, do_reinsuarnce)
+    return (account_df, location_df, ri_info_df, ri_scope_df, do_reinsurance)
 
 
 def run_inuring_level_risk_level(

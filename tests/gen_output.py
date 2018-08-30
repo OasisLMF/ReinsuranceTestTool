605a4f7122673764588b452300361fa77dac34bc
Sam Gamble <hexadessa@gmail.com>
2018-08-30 15:14:10 +0100

    Update test generator

--- tests/gen_output.py
+++ tests/gen_output.py
@@ -6,8 +6,10 @@ import pathlib
 top_level_dir = str(pathlib.Path(__file__).parents[1])
 sys.path.insert(0, top_level_dir)
 
-import reinsurance_tester
 import pandas as pd
+
+import reinsurance_tester
+from oasislmf.exposures import oed
 
 
 output_dir = os.path.join(top_level_dir, 'tests', 'expected')
@@ -60,10 +62,8 @@ case_run_list =  [
 #print(os.listdir(input_dir))
 for case in case_run_list:
     output_location = os.path.join(output_dir, 'calc', case.rsplit('/')[-1])
-
     try:
         os.makedirs(output_location)
-
         try:
             print('[RUNNING]  "{}"'.format(case))
             (
@@ -72,7 +72,7 @@ for case in case_run_list:
                 ri_info_df,
                 ri_scope_df,
                 do_reinsurance
-            ) = reinsurance_tester.load_oed_dfs(case, show_all=False)
+            ) = oed.load_oed_dfs(case, show_all=False)
 
             net_losses = reinsurance_tester.run_test(
                 'run_reinsurance',
@@ -92,8 +92,7 @@ for case in case_run_list:
                 
             print('[SUCCESS]  "{}"'.format(case))
         except Exception as e:
-            print('[FAILED]   "{}"'.format(case))
-            print(e)
+            print('[FAILED]   "{} - {}"'.format(case,e))
 
     except FileExistsError as e:
         print('[SKIPPED]  "{}" dir exisits'.format(case, output_location))

63b6f6b2af9ad2199700a1580c7b09532d5030cd
Joh <johanna.carter@oasislmf.org>
2019-04-11 16:22:33 +0100

    clean up

--- examples/diff.sh
+++ examples/diff.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+# search examples for identical locations file 
 for d in */ ; do
-    diff ../examples_old/simple_QS/ri_scope.csv "$d/ri_scope.csv" > /dev/null
+    diff ../examples_old/simple_QS/location.csv "$d/location.csv" > /dev/null
     retval=$? 
     if [ $retval -eq 0 ]; then
     	echo $d
     fi
 done
+
